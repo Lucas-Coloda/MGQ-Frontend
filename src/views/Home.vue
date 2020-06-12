5073e1b8efe7b58505bc6617803047040556a51d
Lucas Gyan <lg.coloda@gmail.com>
2020-06-12 13:15:10 -0300

    mgq-001 adicionar componentes de login e registro

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>
+      Home
+    </h1>
+    <button @click.prevent="signOut">Sign out</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import firebase from 'firebase';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
+  methods: {
+    signOut: async function () {
+      await firebase.auth().signOut();
+    },
   },
 };
 </script>
